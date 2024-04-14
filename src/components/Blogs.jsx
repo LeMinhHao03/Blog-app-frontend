97ed4113ea63f7815f4a7ad8958d363c2217045b
LeMinhHao03 <haotp252gmail.com>
2024-04-14 21:06:12 +0700

    Show Blog

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import BlogCard from './BlogCard'
 
 const Blogs = () => {
+
+  const [blogs, setBlogs] = useState();
+
+  const fetchBlogs = async () => {
+    const res = await fetch('http://localhost:8000/api/blogs');
+    const result = await res.json();
+    setBlogs(result.data);
+  }
+
+  useEffect(() => {
+    fetchBlogs();
+  },[])
+
   return (
     <div className='container'>
         <div className="d-flex justify-content-between pt-5 mb-4">
           <h4>Blogs</h4>
           <a href='/create' className='btn btn-dark'>Create</a>
         </div>
-        <div className='row'>          
-            <BlogCard/>
-            <BlogCard/>
-            <BlogCard/>
-            <BlogCard/> 
-            <BlogCard/>
-            <BlogCard/>
-            <BlogCard/>
-            <BlogCard/>         
+        <div className='row'>      
+
+          {
+              (blogs) && blogs.map((blog) => {
+                return (<BlogCard blog={blog} key={blog.id}/>)
+              })
+          }
+
+              
+              
         </div>
     </div>
   )
